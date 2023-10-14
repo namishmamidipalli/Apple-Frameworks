b8de3fe18f14a39b40a914f56ca36f7ad2a89af7
namishiiit <namish.mamidipalli@students.iiit.ac.in>
2023-10-14 19:52:43 -0400

    Initial Commit

---
+++ Apple Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple Frameworks
+//
+//  Created by Namish Mamidipalli on 10/14/23.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
